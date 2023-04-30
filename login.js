6bb9cebe72429c5dd7e4d8895b5734ea32c26405
Cuong <cuong.lt204637@sis.hust.edu,vn>
2023-04-30 22:35:46 +0700

    login-form

---
+++ login.js
@@ -0,0 +1,21 @@
+const title = document.querySelectorAll(".Title div")
+const form = document.querySelectorAll(".Form4 form")
+const signup = document.querySelector(".signup_link p")
+const login = document.querySelector(".login_link p")
+const Form = document.querySelector(".Form3")
+
+signup.addEventListener("click", () => {
+    title[0].style.display = "none";
+    title[1].style.display = "block";
+    form[0].style.display = "none";
+    form[1].style.display = "block";
+    Form.style.marginTop = "100px";
+})
+
+login.addEventListener("click", () => {
+    title[1].style.display = "none";
+    title[0].style.display = "block";
+    form[1].style.display = "none";
+    form[0].style.display = "block";
+    Form.style.marginTop = "200px"
+})
